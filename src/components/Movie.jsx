58ba686efea9656a5202698f02b850eeef022203
im20180078 <im20180078@student.fon.bg.ac.rs>
2023-02-08 23:13:46 +0100

    prvi komit

---
+++ src/components/Movie.jsx
@@ -0,0 +1,53 @@
+import React from 'react'; 
+
+import {BsDashLg} from 'react-icons/bs' 
+
+  
+
+function Movie({ film, addToCart, inCart, removeFromCart }) { 
+
+  return ( 
+
+    <div className='card' style={{ margin: 10}}> 
+
+      
+
+      <div className='card-body'> 
+
+        <h3 className='card-title'>{film.title}</h3> 
+
+        <p className='card-text'>{film.synopsys}</p> 
+
+        <br/> 
+
+        <p className='card-text'>Starring: {film.actors}</p> 
+
+      </div> 
+
+       
+
+      {inCart ===1? (<> 
+
+          <button className="btn"  onClick={() => addToCart(film.id)}>Iznajmi</button> 
+
+      </> 
+
+      ):( 
+
+      <> 
+
+          <button className="btn"  onClick={() => removeFromCart(film.id)}>Izbaci</button> 
+
+      </>)} 
+
+    </div> 
+
+  
+
+  ) 
+
+} 
+
+  
+
+export default Movie; 
